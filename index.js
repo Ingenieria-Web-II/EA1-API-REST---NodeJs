be9bb97020dbd34082c9f5c7b930fb82e8a0eef2
Jair Moreno <jairmorenolml@gmail.com>
2025-09-26 01:25:07 -0500

    actualizando index.js

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/db');
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-// Conectar DB
-connectDB();
-
-// Rutas
-app.use('/api/generos', require('./routes/generoRoutes'));
-app.use('/api/directores', require('./routes/directorRoutes'));
-app.use('/api/productoras', require('./routes/productoraRoutes'));
-app.use('/api/tipos', require('./routes/tipoRoutes'));
-app.use('/api/media', require('./routes/mediaRoutes'));
+const app = require('./src/app');
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+
+app.listen(PORT, () => console.log(`🚀 Servidor corriendo en puerto ${PORT}`));
