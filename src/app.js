0b0340221d226d34fbea2fbf0506dd8777ca8544
Jair Moreno <jairmorenolml@gmail.com>
2025-09-26 01:26:40 -0500

    actualizando app.js

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Conectar DB
+connectDB();
+
+// Rutas
+app.use('/api/generos', require('./routes/generoRoutes'));
+app.use('/api/directores', require('./routes/directorRoutes'));
+app.use('/api/productoras', require('./routes/productoraRoutes'));
+app.use('/api/tipos', require('./routes/tipoRoutes'));
+app.use('/api/media', require('./routes/mediaRoutes'));
+
+module.exports = app;
