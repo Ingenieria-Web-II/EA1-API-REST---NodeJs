79743f9e7f3234b7925b390f1fa70a146e4a4f01
Jair Moreno <jairmorenolml@gmail.com>
2025-09-27 20:33:51 -0500

    agregando controlador para la gestión de productoras

---
+++ src/controllers/productoraController.js
@@ -0,0 +1,39 @@
+const Productora = require('../models/Productora');
+
+exports.createProductora = async (req, res) => {
+  try {
+    const productora = await Productora.create(req.body);
+    res.status(201).json(productora);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.getProductoras = async (req, res) => {
+  try {
+    const productoras = await Productora.find();
+    res.json(productoras);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.updateProductora = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const productora = await Productora.findByIdAndUpdate(id, req.body, { new: true });
+    res.json(productora);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.deleteProductora = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const productora = await Productora.findByIdAndUpdate(id, { estado: false }, { new: true });
+    res.json({ message: 'Productora desactivada', productora });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
