de48b0565850241d042d1445e2828b1088345d6e
Jair Moreno <jairmorenolml@gmail.com>
2025-09-27 20:35:26 -0500

    agregando rutas para el manejo de tipos

---
+++ src/routes/tipoRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/tipoController');
+
+router.post('/', controller.createTipo);
+router.get('/', controller.getTipos);
+router.put('/:id', controller.updateTipo);
+router.delete('/:id', controller.deleteTipo);
+
+module.exports = router;
