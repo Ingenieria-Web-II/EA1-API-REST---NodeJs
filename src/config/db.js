178c38e22d4d4102af7ae2867c1bcae8e741960b
Jair Moreno <jairmorenolml@gmail.com>
2025-09-28 20:48:27 -0500

    conexión a MongoDB

--- src/config/db.js
+++ src/config/db.js
@@ -4,9 +4,9 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
     });
-    console.log('✅ MongoDB conectado');
+    console.log('MongoDB conectado');
   } catch (err) {
-    console.error('❌ Error al conectar a MongoDB:', err.message);
+    console.error('Error al conectar a MongoDB :(', err.message);
     process.exit(1);
   }
 };
