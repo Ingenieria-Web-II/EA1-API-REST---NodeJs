ac630857e3002ebcbf38a699a8728c51b9f897a8
Gercod97 <211167201+ProyectosDesarrollo97@users.noreply.github.com>
2025-09-28 22:38:23 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (_, res) => res.send('🚀 API corriendo'));
+app.get('/health', (_, res) => res.json({ ok: true, at: new Date() }));
+
 // Conectar DB
 connectDB();
 
